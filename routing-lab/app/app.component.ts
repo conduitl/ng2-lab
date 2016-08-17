5614f2b9870203b916f77d691589f0cfdc9a9cf1
Thomas <thomas.rundle@conduitlearning.com>
2016-08-16 19:57:21 -0500

    Refactored according to ch5 of tutorial

--- routing-lab/app/app.component.ts
+++ routing-lab/app/app.component.ts
@@ -1,18 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from './hero';
+import { HeroService } from './hero.service';
 
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
+
 @Component({
   selector: 'my-app',
   template: `
@@ -75,11 +65,22 @@ const HEROES: Hero[] = [
       margin-right: .8em;
       border-radius: 4px 0 0 4px;
     }
-  `]
+  `],
+  providers: [HeroService]
 })
 export class AppComponent {
+  constructor(private heroService: HeroService) { }
   title = 'Tour of Heroes';
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
+
+  getHeroes() {
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
+  
+  ngOnInit() {
+    this.getHeroes();
+  }
+
   onSelect(hero: Hero) { this.selectedHero = hero; }
 }

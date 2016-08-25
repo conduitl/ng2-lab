bd19c5c75f869bf3470099d1b0c623df08d818e2
Thomas <thomas.rundle@conduitlearning.com>
2016-08-25 12:38:05 -0500

    Enabled the heroes route --- all works

--- module-lab/app/app.routing.js
+++ module-lab/app/app.routing.js
@@ -1,7 +1,9 @@
 "use strict";
 var router_1 = require('@angular/router');
 exports.routes = [
-    { path: '', redirectTo: 'contact', pathMatch: 'full' } //,
+    { path: '', redirectTo: 'contact', pathMatch: 'full' },
+    // { path: 'crisis', loadChildren: 'app/crisis/crisis.module'},
+    { path: 'heroes', loadChildren: 'app/hero/hero.module' }
 ];
 exports.routing = router_1.RouterModule.forRoot(exports.routes);
 //# sourceMappingURL=app.routing.js.map
--- module-lab/app/app.routing.ts
+++ module-lab/app/app.routing.ts
@@ -1,9 +1,9 @@
 import { Routes, RouterModule } from '@angular/router';
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'contact', pathMatch: 'full'}//,
+    { path: '', redirectTo: 'contact', pathMatch: 'full'},
     // { path: 'crisis', loadChildren: 'app/crisis/crisis.module'},
-    // { path: 'heroes', loadChildren: 'app/hero/hero.module'}
+    { path: 'heroes', loadChildren: 'app/hero/hero.module'}
 ];
 
 export const routing = RouterModule.forRoot(routes);

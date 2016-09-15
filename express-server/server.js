806fbb81561ef884a79df894d9a04980b2785d79
Thomas <thomas.rundle@conduitlearning.com>
2016-09-15 10:09:42 -0500

    Updated file paths in express server

--- express-server/server.js
+++ express-server/server.js
@@ -4,14 +4,14 @@ const http = require('http');
 
 const app = express();
 
-let ngAppPath = path.join(__dirname, '..', 'rc7_quickstart'); 
-let nodeModulesPath = path.join(__dirname, '..', 'rc7_quickstart', 'node_modules');
+let ngAppPath = path.join(__dirname, '..', 'ng2_quickstart'); 
+let nodeModulesPath = path.join(__dirname, '..', 'ng2_quickstart', 'node_modules');
 
 app.use('/', express.static(ngAppPath));
 app.use('/node_modules', express.static(nodeModulesPath));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'rc7_quickstart', 'index.html'));
+    res.sendFile(path.join(__dirname, '..', 'ng2_quickstart', 'index.html'));
 });
 
 http.createServer(app).listen(3000);

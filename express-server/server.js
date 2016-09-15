2b1949aaacca6846e9d6bad8e3103449fa9a3dd5
Thomas <thomas.rundle@conduitlearning.com>
2016-09-14 20:42:51 -0500

    Simplified express static server

--- express-server/server.js
+++ express-server/server.js
@@ -4,18 +4,15 @@ const http = require('http');
 
 const app = express();
 
-let publicPath = path.resolve(__dirname, 'public');
-app.use(express.static(publicPath));
+app.use('/', express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/hello.html'));
+    res.sendFile(path.join(__dirname, 'public', 'hello.html'));
 });
 
 app.use( (req, res) => {
     res.writeHead(200, { "Content-Type": "text/plain" });
     res.end('Looks like you didn\'t find a static file');
 });
-
-
 
 http.createServer(app).listen(3000);
